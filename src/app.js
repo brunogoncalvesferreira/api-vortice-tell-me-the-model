2e5fcbdf223dbab7f509184fec72f05b381575f4
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2025-02-20 13:24:16 -0300

    feat deixando publico

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import { paymentRoutes } from './routes/payment.js'
 export const app = fastify()
 
 await app.register(fastifyCors, {
-  origin: ['https://www.tm3d.com.br', 'http://localhost:8080',],
+  origin: "*",
   credentials: true,
 })
 
